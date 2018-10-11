40f239511ea16bcac956ff936c793f4ae06b23fa
FlorentContreras <florent.contreras@gmail.com>
2018-10-11 17:30:24 +0200

    Premiere iteration d'ajout de dictionnaire (ou autre type) et d'affichage avec print

---
+++ TestClass.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.6
+# -*-coding:utf-8-*
+
+import TP_3_Dictionnaire_ordonne
+
+def testclass():
+	""" Fonction permettrant de tester la class DictionnaireOrdonne
+	"""
+
+
+testclass()
