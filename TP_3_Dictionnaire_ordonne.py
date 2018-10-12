afd0b1ff3a44c26952265209bc694b8d85dd0087
FlorentContreras <florent.contreras@gmail.com>
2018-10-12 10:29:45 +0200

    Ajout méthode getitem

--- TP_3_Dictionnaire_ordonne.py
+++ TP_3_Dictionnaire_ordonne.py
@@ -39,15 +39,85 @@ class DictionnaireOrdonne: # Définition de la class DictionnaireOrdonnee
         print(str(self.cle))
 
 
+    def __getitem__(self, cleAAfficher):
+        """Cette méthode spéciale est appelée quand on fait objet[index]
+        Elle redirige vers self._dictionnaire[index]"""
+
+        print(self.valeur[self.cle.index(cleAAfficher)])
+
+
+    def __setitem__(self, index, valeur):
+        """Cette méthode est appelée quand on écrit objet[index] = valeur
+        On redirige vers self._dictionnaire[index] = valeur"""
+
+        
+
+        #self._dictionnaire[index] = valeur
+
+    def __delitem__(self, cleASupprimer):
+        """ Cette méthode permet de supprimer un couple clé/valeur 
+        à partir de sa clé """
+
+        indice = self.cle.index(cleASupprimer)
+        #del self.valeur.index(indice)
+        #del self.cle.index(indice)
+
 
 """
-On doit pouvoir créer le dictionnaire de plusieurs façons :
+    On doit pouvoir créer le dictionnaire de plusieurs façons :
 
 
+        Pré-rempli grâce à des clés et valeurs passées en paramètre : 
+        comme les dictionnaires usuels, on doit ici avoir la possibilité 
+        de pré-remplir notre objet avec des couples clés-valeurs 
+        passés en paramètre (constructeur(cle1 = valeur1, cle2 = valeur2, …)).
 
 
-    Pré-rempli grâce à des clés et valeurs passées en paramètre : 
-    comme les dictionnaires usuels, on doit ici avoir la possibilité de pré-remplir 
-    notre objet avec des couples clés-valeurs passés en paramètre 
-    (constructeur(cle1 = valeur1, cle2 = valeur2, …)).
-    """
+    Les clés et valeurs doivent être couplées. Autrement dit, 
+    si on cherche à supprimer une clé, la valeur correspondante 
+    doit également être supprimée. Les clés et valeurs se trouvant 
+    dans des listes de même taille, il suffira de prendre l'indice 
+    dans une liste pour savoir quel objet lui correspond dans l'autre. 
+    Par exemple, la clé d'indice 0 est couplée avec la valeur d'indice 0.
+
+
+    On doit pouvoir interagir avec notre objet conteneur grâce aux crochets, 
+    pour récupérer une valeur (objet[cle]), pour la modifier (objet[cle] = valeur) 
+    ou pour la supprimer (del objet[cle]).
+
+
+    Quand on cherche à modifier une valeur, si la clé existe 
+    on écrase l'ancienne valeur, si elle n'existe pas on ajoute le couple clé-valeur 
+    à la fin du dictionnaire.
+
+
+    On doit pouvoir savoir grâce au mot-clé in si une clé se trouve 
+    dans notre dictionnaire (cle in dictionnaire).
+
+
+    On doit pouvoir demander la taille du dictionnaire grâce à la fonction len.
+
+
+    On doit pouvoir afficher notre dictionnaire directement dans l'interpréteur 
+    ou grâce à la fonction print. L'affichage doit être similaire à celui 
+    des dictionnaires usuels ({cle1: valeur1, cle2: valeur2, …}).
+
+
+    L'objet doit définir les méthodes sort pour le trier et reverse pour l'inverser. 
+    Le tri de l'objet doit se faire en fonction des clés.
+
+
+    L'objet doit pouvoir être parcouru. Quand on écrit for cle in dictionnaire, 
+    on doit parcourir la liste des clés contenues dans le dictionnaire.
+
+
+    À l'instar des dictionnaires, trois méthodes keys() 
+    (renvoyant la liste des clés), values() (renvoyant la liste des valeurs) 
+    et items() (renvoyant les couples (clé, valeur)) doivent être mises en œuvre. 
+    Le type de retour de ces méthodes est laissé à votre initiative : 
+    il peut s'agir d'itérateurs ou de générateurs (tant qu'on peut les parcourir).
+
+
+    On doit pouvoir ajouter deux dictionnaires ordonnés (dico1 + dico2) ; 
+    les clés et valeurs du second dictionnaire sont ajoutées au premier."""
+
--- TestClass.py
+++ TestClass.py
@@ -6,17 +6,36 @@ from TP_3_Dictionnaire_ordonne import *
 def testclass():
 	""" Fonction permettrant de tester la class DictionnaireOrdonne """
 
-	print("   Cas 1, dictionnaire vide : ")
+	print("""
+Cas 1, dictionnaire vide : """)
 	dicvide = DictionnaireOrdonne({})
 	print(dicvide)
 
 
-	print("   Cas 2, dictionnaire comportant 2 fruits")
+	print("""
+Cas 2, dictionnaire comportant 2 fruits""")
+
 	extdicfruit = {'pomme': 52, 'poire': 34}
 	fruit = DictionnaireOrdonne(extdicfruit)
 	print(fruit)
 
-	print("   Affichage des clés d'un dictionnaire")
+
+	print("""
+Affichage des clés d'un dictionnaire""")
+
+	fruit.keys()
+
+
+	print("""
+On va afficher la valeur liée à une clé""")
+
+	fruit["pomme"]
+
+
+	print("""
+On va supprimer une clé""")
+
+	del fruit["pomme"]
 	fruit.keys()
 
 
